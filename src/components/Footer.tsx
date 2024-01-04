3f685ecaf631c645ba006a54a79d5818dd6b80c2
Daniel Theuri <dantheuri17@gmail.com>
2024-01-04 21:57:09 +0300

    finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 				<img
 					src={logoLight}
 					alt=""
-					className="object-contain w-[3.97325rem] h-[3.625rem]"
+					className="object-contain w-[10rem] h-[3.625rem]"
 				/>
 			</div>
 			<div className="w-[26.875rem] ml-[8.78rem] ">
@@ -20,7 +20,7 @@ const Footer = () => {
 				</p>
 			</div>
 
-			<div className="flex gap-x-[1rem] h-[1.25rem] ml-[17.5rem] w-[6.25rem]">
+			<div className="flex gap-x-[1.25rem] h-[1.25rem] ml-[17.5rem] w-[6.25rem]">
 				<img src={facebookIcon} alt="" />
 				<img src={instagramIcon} alt="" />
 				<img src={twitterIcon} alt="" />
